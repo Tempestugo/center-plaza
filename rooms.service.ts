7ec0b2dacc8a250ce37a8f512d3b8bd6a9a56db1
Tempestugo <coffeture.blog@gmail.com>
2026-02-15 23:38:54 -0300

    Correção de Path

--- rooms.service.ts
+++ rooms.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { DatabaseService } from '../database/database.service';
+import { DatabaseService } from './database.service';
 
 @Injectable()
 export class RoomsService {
